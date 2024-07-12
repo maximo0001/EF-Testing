4920a02bfd657c541e33a83dd260596e3f092bc3
Maximo Fernandez <maximo148601@gmail.com>
2024-07-12 01:38:28 -0300

    Using EFinMemory db

---
+++ Ef-testing/Ef-testing/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ef_testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext()) 
+            {
+                var purchase = new Purchase()
+                {
+                    Id = 1,
+                    Product = "Hat",
+                    Price = 36.5m
+                };
+                context.Add(purchase);
+            
+                context.SaveChanges();
+            
+            
+                //
+
+                var allPurchases = context.Purchases.ToList();
+            
+            }
+        }
+
+        public class MyDbContext : DbContext
+        {
+            public  DbSet<Purchase> Purchases { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseInMemoryDatabase("MyDb");
+            }
+
+        }
+
+        public class Purchase
+        {
+            public int Id { get; set; }
+            public string? Product { get; set; }
+            public decimal Price { get; set; }
+        }
+    }
+}
---
+++ EntityFramework_1/EntityFramework_1/Program.cs
@@ -0,0 +1,15 @@
+namespace EntityFramework_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
